e4e5962c8466f7caac9f0472b037f2f66db2be7e
Astrr <salamandr55@gmail.com>
2020-04-04 13:45:51 +0300

    Optimisation + Volume slider mute fix

--- Pock/Widgets/ControlCenter/Items/CCVolumeUpItem.swift
+++ Pock/Widgets/ControlCenter/Items/CCVolumeUpItem.swift
@@ -11,6 +11,8 @@ import Defaults
 
 class CCVolumeUpItem: ControlCenterItem {
     
+    var value_corrected: Float = 0
+    
     override var enabled: Bool { return Defaults[.shouldShowVolumeItem] && Defaults[.shouldShowVolumeUpItem] }
     
     private let key: KeySender = KeySender(keyCode: NX_KEYTYPE_SOUND_UP, isAux: true)
@@ -27,14 +29,25 @@ class CCVolumeUpItem: ControlCenterItem {
     }
     
     override func longPressAction() {
-        parentWidget?.showSlideableController(for: self, currentValue: NSSound.systemVolume())
+        parentWidget?.showSlideableController(for: self, currentValue: value_corrected)
     }
     
     override func didSlide(at value: Double) {
-        Defaults[.isVolumeMute] = false
-        NSSound.setSystemVolume(Float(value))
+        //I've noticed that if you try to quickly flick the slider to the left to mute the volume, it often doesn't go all the way to the left which makes the volume really low, but not muted. This part fixes this behaviour by muting the volume when the slider is almost fully to the left.
+        
+        if Float(value)<0.07 {
+            NSSound.setSystemVolume(0.01)
+            value_corrected = 0
+            Defaults[.isVolumeMute] = true
+
+        }
+        else {
+            value_corrected = Float(value)
+            Defaults[.isVolumeMute] = false
+        }
+        NSSound.setSystemVolume(Float(value_corrected))
         NSWorkspace.shared.notificationCenter.post(name: .shouldReloadControlCenterWidget, object: nil)
-        // DK_OSDUIHelper.showHUD(type: NSSound.isMuted() ? .mute : .volume, filled: CUnsignedInt(NSSound.systemVolume() * 16))
+        DK_OSDUIHelper.showHUD(type: NSSound.isMuted() ? .mute : .volume, filled: CUnsignedInt(value_corrected * 16))
     }
     
 }
